be5b2ca38d0b0e608f86bc48d1f6975e14b3c2ba
Pond <pondwachi.th@gmail.com>
2024-05-22 15:41:55 +0700

    faet: skill check point pond

--- question-2.js
+++ question-2.js
@@ -4,3 +4,21 @@ const inventory = [
   { name: "Banana", price: 10, quantity: 50 },
 ];
 // เริ่มเขียนโค้ดตรงนี้
+
+inventory[0].quantity = 200
+
+inventory.push({ name: "Orange", price: 20, quantity: 300,})
+
+
+let totalPrice = 0;
+for (let all of inventory) {
+  totalPrice += all.price * all.quantity;
+}
+
+console.log("มูลค่ารวมของจำนวนสินค้าทั้งหมดในสต็อก: " + (totalPrice) + " " +  "บาท");
+
+
+  
+
+
+
--- question-4.js
+++ question-4.js
@@ -5,3 +5,16 @@ const inventory = [
   { name: "Orange", price: 30, quantity: 60 },
 ];
 // เริ่มเขียนโค้ดตรงนี้
+
+
+let lowerItem = inventory[0];
+
+
+for (let i = 1; i < inventory.length; i++) {
+  if (inventory[i].quantity < lowerItem.quantity)
+    lowerItem = inventory[i]
+  
+}
+
+
+console.log(`สินค้าที่มีจำนวนต่ำที่สุดในคลังสินค้าคือ ${lowerItem.name} ซึ่งมี ${lowerItem.quantity} ชิ้น`);
